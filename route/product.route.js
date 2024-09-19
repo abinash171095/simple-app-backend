5a3afb1e3a63640dd9b609572605a7c7139a6ad5
abinash171095 <135997101+abinash171095@users.noreply.github.com>
2024-09-19 21:02:19 +0530

    second commit

--- route/product.route.js
+++ route/product.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {getProducts,getProduct,createProduct,updateProduct,deleteProduct,} = require('../controller/product.controller.js');
 const { model } = require('mongoose');
 
+
 router.get('/',getProducts);
 router.get('/:id',getProduct);
 router.post('/',createProduct);
